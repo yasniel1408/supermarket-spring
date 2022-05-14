348af58a244d06f76d9d1ef80529bfa63624be23
Yasniel Fajardo Egues <yasnielfajardoegues1408@gmail.com>
2022-05-14 05:09:04 +0200

    first commit

---
+++ src/main/java/com/supermarket/SupermarketSpringApplication.java
@@ -0,0 +1,13 @@
+package com.supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supermarket/SupermarketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
